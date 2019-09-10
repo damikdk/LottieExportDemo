d86e1bd4b4bfef118530bc2006c108b4ca76a064
Damir Minnegalimov <damikdk@gmail.com>
2019-09-10 11:39:54 +0300

    Initial Commit

---
+++ LottieExportDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieExportDemo
+//
+//  Created by Damik Minnegalimov on 10.09.2019.
+//  Copyright © 2019 Damik Minnegalimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
